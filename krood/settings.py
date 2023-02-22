9a1a48030a952e9378374ab60a2c858f348d088d
Paulo Marques <paulo.marques@ubc.ca>
2023-02-22 13:52:55 -0800

    add contrib

--- krood/settings.py
+++ krood/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
